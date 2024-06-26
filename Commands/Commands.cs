fed56fa737c892bff14f62d0f19bb0dcf741f1fd
ram16 <thomass.montaine@gmail.com>
2024-06-26 14:28:02 +0200

    continuer

--- Commands/Commands.cs
+++ Commands/Commands.cs
@@ -6,9 +6,9 @@ namespace _TERMINAL_
 {
     public class Commands : Shell.IUser
     {
-        public delegate void Action<T>(in T arg);
+        public delegate void OnCommand(in string arg0, in LineParser line);
         public static Commands instance;
-        public readonly Dictionary<string, Action<LineParser>> commands = new(StringComparer.OrdinalIgnoreCase);
+        readonly Dictionary<string, OnCommand> commands = new(StringComparer.OrdinalIgnoreCase);
         IEnumerable<string> Shell.IUser.ECommands => commands.Keys;
 
         //----------------------------------------------------------------------------------------------------------
@@ -25,12 +25,19 @@ namespace _TERMINAL_
             Shell.AddUser(instance);
         }
 
+        public static void AddCommand(in string name, in OnCommand onCommand) => instance.commands.Add(name, onCommand);
+        public static void AddCommands(in OnCommand onCommand, params string[] names)
+        {
+            for (int i = 0; i < names.Length; i++)
+                instance.commands.Add(names[i], onCommand);
+        }
+
         //----------------------------------------------------------------------------------------------------------
 
         void Shell.IUser.OnCmdLine(in string arg0, in LineParser line)
         {
-            if (commands.TryGetValue(arg0, out var action))
-                action(line);
+            if (commands.TryGetValue(arg0, out var onCommand))
+                onCommand(arg0, line);
             else
                 Debug.LogError($"{GetType().FullName} Unknown command: \"{arg0}\"");
         }
