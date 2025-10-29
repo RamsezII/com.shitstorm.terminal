030af5fb3016b20ab94c36db4048fec61cf9eaec
ram16 <thomass.montaine@gmail.com>
2025-10-29 16:11:49 +0100

    Update Terminal.cs

--- Runtime/Terminal/Terminal.cs
+++ Runtime/Terminal/Terminal.cs
@@ -52,7 +52,7 @@ namespace _TERMINAL_
 
         protected virtual void OnEnable()
         {
-            UsageManager.ToggleUser(this, true, UsageGroups.TrueMouse, UsageGroups.Keyboard, UsageGroups.Typing, UsageGroups.BlockPlayer);
+            UsageManager.ToggleUser(this, true, UsageGroups.IMGUI, UsageGroups.TrueMouse, UsageGroups.Keyboard, UsageGroups.Typing, UsageGroups.BlockPlayer);
             IMGUI_global.instance.users_ongui.Modify(dict => dict.Add(OnOnGui, this));
         }
 
